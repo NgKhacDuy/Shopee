1d8e6d7e3df39c4b8b994d3aa00b25a12db6d70a
NgKhacDuy <jacobnob673@gmail.com>
2022-07-04 20:50:39 +0700

    add option searching

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,20 @@
-const signInBtn = document.querySelector('.js-sign-up')
-function showFormSignIn(){
-    alert("show")
+const signUpBtn = document.querySelector('.js-sign-up')
+const signInBtn = document.querySelector('.js-sign-in')
+const modalSignUp = document.querySelector('.modal')
+const modalSignUpClose=document.querySelector('.auth-form__controls-back')
+function showFormSignUp() {
+    modalSignUp.classList.add('openSignUpForm')
+}
+function hideFormSignUp() {
+    modalSignUp.classList.remove('openSignUpForm')
 }
 
+function showFormSignIn() {
+    modalSignUp.classList.add('openSignInForm')
+}
+function hideFormSignIn() {
+    modalSignUp.classList.remove('openSignInForm')
+}
 signInBtn.addEventListener('click',showFormSignIn)
+signUpBtn.addEventListener('click', showFormSignUp)
+modalSignUpClose.addEventListener('click',hideFormSignUp)
